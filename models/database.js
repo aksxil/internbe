abf7000f436e275491248c7133b6e2d3974dd749
Aakash Nishad <111107009+aksxil@users.noreply.github.com>
2024-07-07 12:22:03 +0530

    Update database.js

--- models/database.js
+++ models/database.js
@@ -5,8 +5,6 @@ exports.connectDatabase = async () => {
     await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
     });
     console.log("Database Connection Established!");
   } catch (error) {
