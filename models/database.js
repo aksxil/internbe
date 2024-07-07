0993e32ab3b1dd1fda09c8c71f57e98c8d77eb3d
Aakash Nishad <111107009+aksxil@users.noreply.github.com>
2024-07-07 12:16:12 +0530

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,11 +1,23 @@
- const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
- exports.connectDatabase = async ()=>{
-   try {
-    await mongoose.connect(process.env.MONGODB_URL);
-    console.log("Database Connection Eastablished!")
-    
-   } catch (error) {
-    console.log(error.message)
-   }
- }
+exports.connectDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+    console.log("Database Connection Established!");
+  } catch (error) {
+    console.log("Error connecting to the database:", error.message);
+  }
+
+  mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+  mongoose.connection.once('open', () => {
+    console.log('MongoDB connected');
+  });
+};
